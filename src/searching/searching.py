2f4ef338e69f5464914d3c47b407748dbb890b45
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2020-07-06 18:45:42 -0400

    binary_search algo added

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -7,8 +7,23 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
 
     # Your code here
+    while low <= high:
+        mid = (low + high) // 2
+        guess = arr[mid]
 
+        if guess == target:
+            return mid
+        if guess > target:
+            high = mid - 1
+        else:
+            low = mid + 1
 
     return -1  # not found
+
+my_list = [1, 3, 5, 7, 9]
+
+print(binary_search(my_list, 3))
